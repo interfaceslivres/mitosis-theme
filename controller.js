874b9a55f5b2b36207abd17031353734bd706619
angelobritop <angelo_brito_p@hotmail.com>
2023-05-04 11:18:58 -0300

    Responsividade ponta e add o menu do header

---
+++ controller.js
@@ -0,0 +1,10 @@
+// função para menu responsivo
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
