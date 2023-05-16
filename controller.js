4b1a77aa19e25b036ffb8007ba1c42ac2e75709f
angelobritop <angelo_brito_p@hotmail.com>
2023-05-16 11:04:32 -0300

    Test de accordion

--- controller.js
+++ controller.js
@@ -8,3 +8,25 @@ function menuTransforma() {
       x.className = "topnav";
     }
   }
+
+  const accBtns = document.querySelectorAll('.acc-btn');
+
+  accBtns.forEach(function(accBtn) {
+    accBtn.addEventListener('click', function() {
+      const accContent = this.nextElementSibling;
+  
+      if (accContent.style.display === 'none') {
+        const selected = 'selected';
+        const allAccContent = document.querySelectorAll('.acc-content');
+  
+        allAccContent.forEach(function(content) {
+          content.style.display = 'none';
+        });
+  
+        accContent.style.display = 'block';
+        accContent.style.transition = selected;
+      } else {
+        accContent.style.display = 'none';
+      }
+    });
+  });
