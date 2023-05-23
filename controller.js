76205c1572ec61850d6ebaa0b8cf34b158f0dc37
angelobritop <angelo_brito_p@hotmail.com>
2023-05-23 11:23:20 -0300

    Resolvendo bug do menu responsivo

--- controller.js
+++ controller.js
@@ -1,32 +1,40 @@
 // função para menu responsivo
 
 function menuTransforma() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
 
-  const accBtns = document.querySelectorAll('.acc-btn');
+const accBtns = document.querySelectorAll('.acc-btn');
 
-  accBtns.forEach(function(accBtn) {
-    accBtn.addEventListener('click', function() {
-      const accContent = this.nextElementSibling;
-  
-      if (accContent.style.display === 'none') {
-        const selected = 'selected';
-        const allAccContent = document.querySelectorAll('.acc-content');
-  
-        allAccContent.forEach(function(content) {
-          content.style.display = 'none';
-        });
-  
-        accContent.style.display = 'block';
-        accContent.style.transition = selected;
-      } else {
-        accContent.style.display = 'none';
-      }
-    });
+accBtns.forEach(function(accBtn) {
+  accBtn.addEventListener('touchstart', function() {
+    const accContent = this.nextElementSibling;
+
+    if (accContent.style.display === 'none') {
+      const selected = 'selected';
+      const allAccContent = document.querySelectorAll('.acc-content');
+
+      allAccContent.forEach(function(content) {
+        content.style.display = 'none';
+      });
+
+      accContent.style.display = 'block';
+      accContent.style.transition = 'selected';
+    } else {
+      accContent.style.display = 'none';
+    }
   });
+});
+
+
+window.addEventListener('resize', function(event) {  
+  if (this.screen.width > 813) {
+  var x = document.getElementById("myTopnav");
+  x.className = "topnav";
+  };
+}, true);
