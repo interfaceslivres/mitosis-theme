0c08565d51f82b97a065e0b97e6db38aa8027510
unknown <paulohsms@gmail.com>
2023-08-15 11:18:14 -0300

    transformando em tema parte 1

---
+++ header.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Estudo Site Coordenação</title>
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
+
+  <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://fonts.cdnfonts.com/css/rawline" rel="stylesheet">
+
+  <link href="assets/fontawesome6/css/fontawesome.css" rel="stylesheet">
+  <link href="assets/fontawesome6/css/brands.css" rel="stylesheet">
+  <link href="assets/fontawesome6/css/regular.css" rel="stylesheet">
+  <link href="assets/fontawesome6/css/solid.css" rel="stylesheet">
+	<?php wp_head(); ?>
+</head>
+<body>
+
+<div class="container">
+  <!-- Lateral esquerda do site-->
+  <div class="lateral">
+    <div class="logo-bg">
+    	<div class="logo">
+    		<img  src="img/midias-logo.png" alt="logo DEMID">
+    	</div>
+    </div>
+      <div class="sidebar-bg">
+        <div class="sidebar">
+          <ul>
+            <li class="sidebar-titulo">Docentes</li>
+            <li><a href="#">notícias</a></li>
+            <li><a href="#">corpo adminstrativo</a></li>
+            <li class="sidebar-titulo">Discentes</li>
+            <li><a href="#">tcc</a></li>
+            <li><a href="#">documentos</a></li>
+            <li><a href="#">projetos</a></li>
+            <li><a href="#">notícias</a></li>
+            <li><a href="#">mapa</a></li>
+            <li class="sidebar-titulo">Visitantes</li>
+            <li><a href="#">apresentação</a></li>
+            <li><a href="#">vídeos</a></li>
+            <li><a href="#">mapa</a></li>
+            <li class="sidebar-titulo">Links</li>
+            <li><a href="#">sigaa</a></li>
+            <li><a href="#">ufpb</a></li>
+          </ul>
+        </div>
+	    </div>
+  </div>
+
+
+	  <!-- Lateral direita do site-->
+	  <header class="cabecalho">
+
+	    <div class="titulo">
+
+	      <div class="tit_img"><img id="tit_img" src="img/SVG final.svg" alt="Logo do curso de Comunicação em Mídias Digitais"></div>
+
+	    	<div class="title">
+	    		<p id="tit_centro">CENTRO DE CIÊNCIAS HUMANAS, LETRAS E ARTES</p>
+	        <h1 id="tit_curso">Comunicação em Mídias Digitais</h1>
+	    	</div>
+
+	    	<div class="brasao">
+	        <img  src="img/SVG final.svg" alt="logo ufpb">
+	      </div>
+	    </div>
+	    <div class="nav-bar-bg">
+	    	<div class="nav-bar">
+	        <div class="topnav" id="myTopnav">
+	            <nav>
+	              <ul>
+	                <li><a href="#">Notícias</a></li>
+	                <li><a href="#">Curso</a></li>
+	                <li><a href="#">Projetos</a></li>
+	                <li><a href="#">Documentos</a></li>
+	                <li><a href="#">Chamados</a></li>
+	                <li>
+	                  <div class="acc-container">
+
+	                    <div class="acc-btn"><a>Docentes</a><i class="fa-solid fa-chevron-down"></i></div>
+	                    <div class="acc-content">
+	                      <a href="#">notícias</a>
+	                      <a href="#">corpo adminstrativo</a>
+	                    </div>
+
+	                    <div class="acc-btn"><a>Discentes</a><i class="fa-solid fa-chevron-down"></i></div>
+	                    <div class="acc-content">
+	                      <a href="#">tcc</a>
+	                      <a href="#">documentos</a>
+	                      <a href="#">projetos</a>
+	                      <a href="#">notícias</a>
+	                      <a href="#">mapa</a>
+	                    </div>
+
+	                    <div class="acc-btn"><a>Visitantes</a><i class="fa-solid fa-chevron-down"></i></div>
+	                    <div class="acc-content">
+	                      <a href="#">apresentação</a>
+	                      <a href="#">vídeos</a>
+	                      <a href="#">mapa</a>
+	                    </div>
+
+	                    <div class="acc-btn"><a> Links</a><i class="fa-solid fa-chevron-down"></i></div>
+	                    <div class="acc-content">
+	                      <a href="#">sigaa</a>
+	                      <a href="#">ufpb</a>
+	                    </div>
+
+	                  </div>
+	                </li>
+	              </ul>
+	            </nav>
+
+	            <a href="javascript:void(0);" class="icon" onclick="menuTransforma()"><i id="menuParaFechar" class="fa-solid fa-bars"></i><p class="menuAbrir" id="menuMenu">Menu</p><p class="menuFechar" id="menuFecharId">Fechar</p></a>
+	      </div>
+
+	      <div class="acessibilidade">
+	        <a href="javascript:void(0);" class="iconAcessibilidade" onclick="test()" ><i id="acessibilidadeParaFechar" class="fa-solid fa-universal-access"></i></a>
+	      </div>
+
+	      <div class="lupa">
+	        <a href="javascript:void(0);" class="iconPesquisa"  onclick="menuTransformaPesquisa()" ><i id="lupaParaFechar" class="fa-solid fa-magnifying-glass"></i></a>
+	      </div>
+
+	      <div id="myPesquisa" class="pesquisa" >
+	        <div class="barraBotaop">
+	          <div class="barra">
+	            <input type="text" placeholder="Digite sua pesquisa..." id="barraConfig">
+	          </div>
+	          <div class="botaop">
+	            <p>Pesquisar</p>
+	            <i id="xBarra" class="fa-solid fa-magnifying-glass"></i>
+	          </div>
+	        </div>
+	      </div>
+
+	    	</div>
+	    </div>
+	  </header>
---
+++ index.php
@@ -0,0 +1,103 @@
+
+<?php get_header(); ?>
+
+  <div class="main">
+    <div class="noticias">
+      <h1 class="secoes">Notícias</h1>
+      <div class="conteudo">
+        <div class="noticias-coluna-unica">
+          <a class="noticia-sem-img camada-1" href="#">
+            <div class="rotulo">
+              <div>15 de Setembro de 2022</div>
+              <div>Notícia, Cataclisma</div>
+            </div>
+            <div class="noticia-sem-img-titulo" id="noticia-principal">Resultado final do concurso de "Áudio Digital"</div>
+          </a>
+        </div>
+        <div class="noticias-coluna">
+          <a href="#" class="noticia-com-img camada-1" style="
+          background-image:
+          linear-gradient(180deg, rgba(0,   0,   0, 0.5) 0%, rgba(0, 0, 0, 0) 50%),
+          linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, #ffffff 85%),
+          url('img/noticia.jpeg')">
+            <div class="rotulo">
+              <div>15 de Setembro de 2022</div>
+              <div>Notícia, Cataclisma</div>
+            </div>
+            <div class="noticia-com-img-titulo">Resultado final do concurso de "Marketing Digital e Empreendedorismo"</div>
+          </a>
+          <a href="#" class="noticia-sem-img camada-1">
+            <div class="rotulo">
+              <div>15 de Setembro de 2022</div>
+              <div>Notícia, Cataclisma</div>
+            </div>
+            <div class="noticia-sem-img-titulo">Resultado final do concurso de "Áudio Digital"</div>
+          </a>
+        </div>
+      </div>
+        <a class="mais-link" href="#">Mais Notícias</a>
+    </div>
+
+    <div class="curso">
+      <h2 class="secoes">Comunicação em Mídias digitais</h2>
+
+      <div class="youtube"><iframe width="100%" height="100%"
+         src="https://www.youtube.com/embed/XUCxGYSJEDY?autoplay=0&mute=1"
+         title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; web-share" allowfullscreen></iframe>
+      </div>
+
+      <div class="text-curso">
+
+        <h3>O que faz um Comunicador em Mídias Digitais?</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
+
+        <h3>Atribuições em Comunicador em Mídias Digitais?</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
+
+        <h3>Mercado de trabalho para um Comunicador em Mídias Digitais?</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
+
+
+      </div>
+    </div>
+    <div class="mais">
+      <div class="mapa">
+
+        <div class="mapa-titulo">
+          <h2>Mapa</h2>
+        </div>
+
+        <div class="mapa-img">
+          <iframe scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d111.87252228998113!2d-34.84983169399343!3d-7.137336889729705!2m3!1f22.312500000000025!2f48.49688112803378!3f0!3m2!1i1024!2i768!4f35!3m3!1m2!1s0x7acc2b97fcc474f%3A0x7330f1f2dbd68687!2sDEMID - Departamento de Mídias Digitais!5e1!3m2!1sen!2sbr!4v1682431792367!5m2!1sen!2sbr" width="100%" height="100%" frameborder="0"><a href="https://www.maps.ie/distance-area-calculator.html">area maps</a></iframe>
+        </div>
+
+      </div>
+
+      <div class="text-ufpb">
+
+        <div class="text-ufpb-titulo">
+          <h2>UFPB</h2>
+        </div>
+
+        <div class="text-ufpb-text">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
+        </div>
+
+        <div class="text-ufpb-link">
+          <a class="mais-link" href="#">Apresentação</a>
+          <a class="mais-link" href="#">Projeto Político-Pedagógico</a>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+</div>
+
+<div class="imagem-curso">
+  <img src="img/banner.jpg" alt="frente predio DEMID">
+</div>
+
+
+
+<?php get_footer(); ?>
