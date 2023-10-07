74829b7825e7d5648483d58fa69d0491a6f080a0
Interfaces Livres <paulohsms@gmail.com>
2023-10-06 21:10:40 -0300

    criacao de um widget personalizado

--- functions.php
+++ functions.php
@@ -1,7 +1,10 @@
 <?php
-	
+
+// Adicionar suporte p/ Thumbnails
 add_theme_support( 'post-thumbnails' );
 
+
+// Registrar os Menus do Tema
 function register_menus() { 
     register_nav_menus(
         array(
@@ -14,6 +17,9 @@ function register_menus() {
 }
 add_action( 'init', 'register_menus' );
 
+
+
+// Função da logo customizada
 function themename_custom_logo_setup() {
 	$defaults = array(
 
@@ -25,5 +31,182 @@ function themename_custom_logo_setup() {
 	add_theme_support( 'custom-logo', $defaults );
 }
 add_action( 'after_setup_theme', 'themename_custom_logo_setup' );
-?>
 
+
+
+
+
+// Registrar widgets
+function registrar_widgets_personalizados() {
+    // Registrar um widget personalizado
+    register_sidebar(array(
+        'name'          => 'Área de Widgets da Home',
+        'id'            => 'widgets-da-home',
+        'description'   => 'Esta é a descrição da minha área de widget personalizada.',
+        'before_widget' => '',
+        'after_widget'  => '',
+        'before_title'  => '',
+        'after_title'   => '',
+    ));
+
+    // Registrar mais widgets personalizados, se necessário
+}
+
+// Hook para registrar os widgets personalizados
+add_action('widgets_init', 'registrar_widgets_personalizados');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Registrar o widget personalizado
+function registrar_widget_curso() {
+    register_widget('Widget_Curso');
+}
+add_action('widgets_init', 'registrar_widget_curso');
+
+// Criar a classe do widget personalizado
+class Widget_Curso extends WP_Widget {
+    public function __construct() {
+        parent::__construct(
+            'widget_curso',
+            'Widget de Curso',
+            array(
+                'description' => 'Um widget personalizado para cursos com campos de nome, vídeo, FAQ, localização, link destaque e imagem banner.'
+            )
+        );
+    }
+
+    // Função para exibir o widget no frontend
+    public function widget($args, $instance) {
+        echo $args['before_widget'];
+        // Exibir o conteúdo do widget
+        echo '<h2 class="secoes">' . esc_html($instance['nome-do-curso']) . '</h2>';
+        if (!empty($instance['video-do-curso'])) {
+	        $url = esc_url($instance['video-do-curso']);
+	        // Substitua "watch" por "embed" na URL
+	        $embed_url = str_replace("watch?v=", "embed/", $url);
+	        echo '<div class="youtube"><iframe width="100%" height="100%" src="' . $embed_url . '" title="Video Player" frameborder="0" allow="web-share" allowfullscreen></iframe></div>';
+    	}
+        // FAQ
+        echo '<div class="text-curso">';
+        for ($i = 1; $i <= 3; $i++) {
+            $faq = $instance['faq' . $i];
+            $resposta = $instance['resposta' . $i];
+            if (!empty($faq) && !empty($resposta)) {
+                echo '<h3>' . esc_html($faq) . '</h3><p>' . esc_html($resposta) . '</p>';
+            }
+        }
+        echo '</div> <!-- fecha div text-curso --> </div> <!-- fecha div curso -->';
+
+        // Localização
+        if (!empty($instance['localizacao'])) {
+
+
+				// URL do Google Maps
+				$google_maps_url = esc_html($instance['localizacao']);
+				if (preg_match('/@([-0-9.]+),([-0-9.]+)/', $google_maps_url, $matches)) {
+				    $latitude = $matches[1];
+				    $longitude = $matches[2];
+				    
+				    // Crie o código de incorporação do Google Maps
+				    $iframe_code = '<iframe src="https://maps.google.com/maps?q='.$latitude.','.$longitude.'&t=k&hl=pt-BR&z=20&amp;output=embed" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+				}
+
+
+
+	        echo '<div class="mais"> <div class="mapa"> <div class="mapa-titulo">';
+	        echo '<h2>Localização:</h2></div>';
+	        echo '<div class="mapa-img">' . $iframe_code . '</div></div>';
+	        echo '<div class="text-ufpb"> <div class="text-ufpb-text"> <p>' . esc_html($instance['texto-localizacao']) . '</p></div>';
+    	
+
+	        // Links de Destaque
+	        echo '<div class="text-ufpb-link">';
+	        for ($i = 1; $i <= 2; $i++) {
+	            $texto_link = $instance['texto-link-destaque' . $i];
+	            $url_link = $instance['url-link-destaque' . $i];
+	            if (!empty($texto_link) && !empty($url_link)) {
+	                echo '<a class="mais-link" href="' . esc_url($url_link) . '">' . esc_html($texto_link) . '</a>';
+	            }
+	        }
+	    }
+
+        echo '</div> </div> </div> </div> </div></div> </div>  <!-- fecha todas as divs do conteiner -->';
+
+        // Imagem Banner
+        if (!empty($instance['imagem-banner'])) {
+            echo '<div class="imagem-curso"> <img src="' . esc_url($instance['imagem-banner']) . '" alt="Adendo gráfico do site do curso"></div>';
+        }
+
+        echo $args['after_widget'];
+    }
+
+    // Função para exibir o formulário de configuração do widget no painel de controle
+    public function form($instance) {
+        // Campos do widget
+        $campos = array(
+            'nome-do-curso' => 'Nome do Curso',
+            'video-do-curso' => 'Vídeo do Curso',
+            'faq1' => 'Informação 1',
+            'resposta1' => 'Resposta 1 (670 caracteres)',
+            'faq2' => 'Informação 2',
+            'resposta2' => 'Resposta 2 (670 caracteres)',
+            'faq3' => 'Informação 3',
+            'resposta3' => 'Resposta 3 (670 caracteres)',
+            'localizacao' => 'Localização (cole apenas a URL do Google Maps)',
+            'texto-localizacao' => 'Texto sobre Localização (670 caracteres)',
+            'texto-link-destaque1' => 'Texto do Link Destaque 1',
+            'url-link-destaque1' => 'URL do Link Destaque 1',
+            'texto-link-destaque2' => 'Texto do Link Destaque 2',
+            'url-link-destaque2' => 'URL do Link Destaque 2',
+            'imagem-banner' => 'URL da Imagem Banner'
+        );
+
+		// Exibir campos do formulário
+		$index = 0;
+		foreach ($campos as $campo => $label) {
+		    $valor = !empty($instance[$campo]) ? esc_attr($instance[$campo]) : '';
+		    echo '<p>';
+		    echo '<label for="' . $this->get_field_id($campo) . '">' . esc_html($label) . ':</label>';
+
+		    // Verificar se o índice é par
+		    if ($index < 3 || $index == 4 || $index == 6 || $index == 8 ||  $index >= 10) {
+		        echo '<input class="widefat" id="' . $this->get_field_id($campo) . '" name="' . $this->get_field_name($campo) . '" type="text" value="' . $valor . '">';
+		    } else {
+		        echo '<textarea class="widefat"  maxlength="677" id="' . $this->get_field_id($campo) . '" name="' . $this->get_field_name($campo) . '" rows="5">' . $valor . '</textarea>';
+		    }
+
+		    echo '</p>';
+		    $index++;
+		}
+    }
+
+    // Função para atualizar os valores do widget no painel de controle
+    public function update($new_instance, $old_instance) {
+        $instance = array();
+        foreach ($new_instance as $campo => $valor) {
+            $instance[$campo] = (!empty($valor)) ? strip_tags($valor) : '';
+        }
+        return $instance;
+    }
+}
+
+
+
+
+
+
+
+
+
+?>
--- index.php
+++ index.php
@@ -251,73 +251,11 @@
 
 
 
-
-
-
-
-
-
-
     <div class="curso">
-      <h2 class="secoes">Comunicação em Mídias digitais</h2>
-
-      <div class="youtube"><iframe width="100%" height="100%"
-         src="https://www.youtube.com/embed/XUCxGYSJEDY?autoplay=0&mute=1"
-         title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; web-share" allowfullscreen></iframe>
-      </div>
-
-      <div class="text-curso">
-
-        <h3>O que faz um Comunicador em Mídias Digitais?</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
-
-        <h3>Atribuições em Comunicador em Mídias Digitais?</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
-
-        <h3>Mercado de trabalho para um Comunicador em Mídias Digitais?</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
 
 
-      </div> <!-- fecha div text-curso -->
-    </div> <!-- fecha div curso -->
-    <div class="mais">
-      <div class="mapa">
+<?php $widget_values = dynamic_sidebar('widgets-da-home'); ?>
 
-        <div class="mapa-titulo">
-          <h2>Mapa</h2>
-        </div>
-
-        <div class="mapa-img">
-          <iframe scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d111.87252228998113!2d-34.84983169399343!3d-7.137336889729705!2m3!1f22.312500000000025!2f48.49688112803378!3f0!3m2!1i1024!2i768!4f35!3m3!1m2!1s0x7acc2b97fcc474f%3A0x7330f1f2dbd68687!2sDEMID - Departamento de Mídias Digitais!5e1!3m2!1sen!2sbr!4v1682431792367!5m2!1sen!2sbr" width="100%" height="100%" frameborder="0"><a href="https://www.maps.ie/distance-area-calculator.html">area maps</a></iframe>
-        </div>
-
-      </div>
-
-      <div class="text-ufpb">
-
-        <div class="text-ufpb-titulo">
-          <h2>UFPB</h2>
-        </div>
-
-        <div class="text-ufpb-text">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
-        </div>
-
-        <div class="text-ufpb-link">
-          <a class="mais-link" href="#">Apresentação</a>
-          <a class="mais-link" href="#">Projeto Político-Pedagógico</a>
-        </div>
-      </div>
-
-    </div> <!-- fecha div mais -->
-
-  </div> <!-- fecha div main -->
-</div> <!-- fecha div misterio -->
-
-<div class="imagem-curso">
-  <img src="img/banner.jpg" alt="frente predio DEMID">
 </div>
-
-
 
 <?php get_footer(); ?>
