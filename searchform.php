17d4a37bca135546e99d2cb1f226801175e7eea2
Interfaces Livres <paulohsms@gmail.com>
2023-12-05 11:32:51 -0300

    search de angelo

--- searchform.php
+++ searchform.php
@@ -1,7 +1,8 @@
 
+
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
     <div>
         <input type="text" value="" name="s" id="s" placeholder="Pesquisa:" />
-        <input type="submit" id="searchsubmit" value="Search" />
+        <button type="submit" id= "xBarra"  class="fa-solid fa-magnifying-glass" > </button>
     </div>
 </form>
